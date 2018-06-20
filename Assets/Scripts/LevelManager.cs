168a60ddaa7380b76ae2aa6b0dd03b1147a6f7d5
MiskaRantala <miska.rantala@gmail.com>
2018-06-20 14:30:27 +0300

    Create level with tiles

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,13 +4,57 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
 
+    /// <summary>
+    /// A prefab for creating a single tile
+    /// </summary>
+    [SerializeField]
+    private GameObject tile;
+
+    /// <summary>
+    /// Calculates the size and returns it as a float
+    /// </summary>
+    public float TileSize
+    {
+        get { return tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        CreateLevel();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    /// <summary>
+    /// Creates our level
+    /// </summary>
+    private void CreateLevel()
+    {
+
+        for (int x = 0; x < 20; x++) //y position
+        {
+            for (int y = 0; y < 20; y++) //x position
+            {
+                PlaceTile(x, y);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Places the tiles where we want
+    /// </summary>
+    private void PlaceTile(int x, int y)
+    {
+        //Creates a new tile and makes a reference to that tile in the NewTile
+        GameObject newTile = Instantiate(tile);
+
+        //Uses the new tile variable to change the position of the tile
+        newTile.transform.position = new Vector3(TileSize * x, TileSize * y, 0);
+    }
+
 }
