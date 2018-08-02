55ba8f8cde1a5e231f87e39cea5ee644b737b6f2
Joni Mettälä <joni.mettala@gmail.com>
2018-08-02 13:47:13 +0300

    Add colliding to Projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -37,4 +37,12 @@ public class Projectile : MonoBehaviour {
             GameManager.Instance.Pool.ReleaseObject(gameObject);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Monster")
+        {
+            Debug.Log("Monster hit");
+        }
+    }
 }
