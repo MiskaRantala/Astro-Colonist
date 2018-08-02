3aa057b956a2a941fc9e0a8eea20eb2f41f85963
Joni Mettälä <joni.mettala@gmail.com>
2018-08-02 14:58:31 +0300

    Fix and add stuff
    - Fixes monster health prefabs.
    - Adds scripts related to health bars.

---
+++ Assets/Scripts/Health/BarScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private Image content;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        HandleBar();
+	}
+
+    private void HandleBar()
+    {
+        content.fillAmount = fillAmount;
+    }
+}
